95b0c6b89ff828772ba5df461706d646ef7abead
Jacob L. Steenwyk <jlsteenwyk@gmail.com>
2020-11-04 09:49:25 -0600

    added theme_big_grid()

---
+++ R/theme_big_grid.R
@@ -0,0 +1,32 @@
+#' Grid theme extension for ggplot2 plots
+#'
+#' @description
+#' Grid theme for ggplot2 plots. See \href{https://ggplot2.tidyverse.org/reference/theme.html}{theme()} for complete documentation.
+#' 
+#' @export
+theme_grid <- function () { 
+    theme_bw(base_size=12, base_family="") %+replace% 
+        theme(
+            plot.background = element_rect(fill="transparent", colour=NA), 
+            legend.background = element_rect(fill="transparent", colour=NA),
+            legend.key = element_rect(fill="transparent", colour=NA),
+            legend.title=element_text(size=24),
+            legend.text=element_text(size=20),
+            axis.line = element_line(color = "black", size = .75, linetype = "solid"),
+            axis.ticks = element_line(colour = "black", size = .75),
+            panel.background  = element_blank(),
+            panel.grid.minor = element_blank(),
+            panel.grid.major = element_line(color="lightgrey"),
+            panel.border = element_blank(),
+            legend.position = "bottom",
+            plot.title = element_text(size=16, hjust = 0.0, vjust = 1.75),
+            axis.text.x = element_text(color="black", size=20, margin = margin(t = 4, r = 0, b = 0, l = 0)),
+            axis.text.y = element_text(color="black", size=20, margin = margin(t = 0, r = 4, b = 0, l = 0)),
+            axis.title.y = element_text(margin = margin(t = 0, r = 10, b = 0, l = 0), angle = 90, size = 14),
+            axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0), angle = 0, size = 14),
+            axis.ticks.length = unit(0.20, "cm"),
+            strip.background = element_rect(color="black", size=.75, linetype="solid"),
+            strip.text.x = element_text(size = 20, color = "black"),
+            strip.text.y = element_text(size = 20, color = "black")
+        )
+}
