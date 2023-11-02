fd6b5e748479ed5ec29a6cadd046f771fd9791ea
JLSteenwyk <jlsteenwyk@gmail.com>
2023-11-02 09:28:33 -0700

    added new color palettes

--- R/colors.R
+++ R/colors.R
@@ -14,7 +14,12 @@ friendly_pals <- list(
   vibrant_seven = c("#0077BB", "#EE7733", "#33BBEE", "#CC3311", "#009988", "#EE3377", "#BBBBBB"),
   muted_nine = c("#332288", "#117733", "#CC6677", "#88CCEE", "#999933", "#882255", "#44AA99", "#DDCC77", "#AA4499"),
   nickel_five = c("#648FFF", "#FE6100", "#785EF0", "#FFB000", "#DC267F"),
-  ito_seven = c("#0072B2", "#D55E00", "#009E73", "#CC79A7", "#56B4E9", "#E69F00", "#F0E442")
+  ito_seven = c("#0072B2", "#D55E00", "#009E73", "#CC79A7", "#56B4E9", "#E69F00", "#F0E442"),
+  ibm_five = c("#648FFF", "#785EF0", "#DC267F", "#FE6100", "#FFB000"),
+  wong_eight = c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#000000"),
+  tol_eight = c("#332288", "#117733", "#44AA99", "#88CCEE", "#DDCC77", "#CC6677", "#AA4499", "#882255"),
+  zesty_four = c("#F5793A", "#A95AA1", "#85C0F9", "#0F2080"),
+  retro_four = c("#601A4A", "#EE442F", "#63ACBE", "#F9F4EC")
 )
 
 #' A color blind friendly color palette generator
